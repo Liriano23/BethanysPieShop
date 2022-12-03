86c0ad8d01c8632f1672a27b531e246a74b0fb3b
Enmanuel Gonzalez Liriano <engonzalez@evopointsolution.com>
2022-12-03 17:11:50 -0400

    Add models and mocked repository

---
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategory =>
+            new List<Category>
+            {
+                new Category { CategoryId = 1, CategoryName = "Fruit pies", Description = "All-fruits" },
+                new Category { CategoryId = 2, CategoryName = "Cheese cakes", Description = "Cheesy" },
+                new Category { CategoryId = 3, CategoryName = "Seasonal pies", Description = "Get in" }
+            };
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,3 +1,4 @@
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,6 +15,9 @@ namespace BethanysPieShop
             //These service injection add support for MVC
             services.AddControllersWithViews();
             services.AddMvc();
+
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddScoped<IPieRepository, MockPieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
